63f9710f34ce1e979101363bc1c0f2b948e34055
endersonpb <endersonpb0@gmail.com>
2023-12-07 17:26:16 -0300

    update

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
-def test_root_deve_retornar_200_e_ola_mundo(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+# def test_root_deve_retornar_200_e_ola_mundo(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+#     assert response.json() == {'message': 'Olá Mundo!'}
 
 
 def test_create_user(client):
